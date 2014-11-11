f31e75dac137407c2865849934ca881170232cf0
Andrei Chugunov <admin@pythagor.com>
2014-11-11 19:56:12 +0400

    Fix Yii instance

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,6 @@
 <?php
-
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require(__DIR__ . '/../vendor/autoload.php');
 }
 
-class Yii extends \yii\BaseYii
-{
-
-}
-
-spl_autoload_register(['Yii', 'autoload'], true, true);
-Yii::$classMap = include('vendor/yiisoft/yii2' . '/classes.php');
-Yii::$container = new yii\di\Container;
+require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
